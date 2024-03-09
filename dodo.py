63408786aa177ebc59e28b2574bc3f181f3e4033
Fernando Rocha Urbano <fernando.rocha.urbano@gmail.com>
2024-03-08 23:45:31 -0500

    changed tests

--- dodo.py
+++ dodo.py
@@ -10,11 +10,10 @@ from pathlib import Path
 from doit.tools import run_once
 from src.data_download import download_and_extract_zip
 from src.data_download import URLS
-from src.data_loader import convert_tsf_to_dataframe
-from src.data_loader import generate_table1_dataframe
+from src.tables_create import convert_tsf_to_dataframe
+from src.tables_create import generate_table1_dataframe
 from src.tables_to_latex import upload_table_download_latex
 from src.test_data_download import test_data_download
-from src.test_table1_csv import test_table1_results 
 
 BASE_DIR = Path(config.BASE_DIR)
 OUTPUT_DIR = Path(config.OUTPUT_DIR)
@@ -58,7 +57,7 @@ def task_generate_table1():
     return {
         'actions': [(generate_table1_dataframe, [DATA_DIR])],
         'targets': [BASE_DIR / 'results' / 'tables' / 'table1.csv'],
-        'uptodate': [True],  # Force to generate table1 every time
+        'uptodate': [True],  # Force re-download every time if equals to False
         'clean': True,
         'verbosity': 0
     }
@@ -68,7 +67,7 @@ def task_transform_table1_to_latex():
     """Generate table1.csv from the downloaded data."""
     return {
         'actions': [(upload_table_download_latex, ['output/tables/table1.csv', 'table1'])],
-        "file_dep": ["./reports/report.tex"],
+        "file_dep": [BASE_DIR / 'output' / 'tables' / 'table1.csv'],
         'targets': [BASE_DIR / 'results' / 'tables' / 'table1.tex'],
         'uptodate': [False],  # Force to generate table1 every time
         'clean': True,
@@ -85,5 +84,6 @@ def task_compile_latex_docs():
         ],
         "targets": ["./reports/report.pdf"],
         "file_dep": ["./reports/report.tex"],
+        'uptodate': [False],  # Force to generate table1 every time
         "clean": True,
     }
