02f7cdbe6ffc65a8ad65b8d4d6de98dd17ee8484
Fernando Rocha Urbano <fernando.rocha.urbano@gmail.com>
2024-03-09 02:30:37 -0500

    added latex of table 2

--- dodo.py
+++ dodo.py
@@ -11,7 +11,7 @@ from doit.tools import run_once
 from src.data_download import download_and_extract_zip
 from src.data_download import URLS
 from src.tables_create import convert_tsf_to_dataframe
-from src.tables_create import generate_table1_dataframe
+from src.tables_create import generate_table1_dataframe, generate_table2_dataframe
 from src.tables_to_latex import upload_table_download_latex
 from src.test_data_download import test_data_download
 
@@ -59,6 +59,16 @@ def task_generate_table1():
         'targets': [BASE_DIR / 'results' / 'tables' / 'table1.csv'],
         'uptodate': [True],  # Force re-download every time if equals to False
         'clean': True,
+    }
+
+
+def task_generate_table2():
+    """Generate table2.csv from the downloaded data."""
+    return {
+        'actions': [(generate_table2_dataframe, ['Mean MASE'])],
+        'targets': [BASE_DIR / 'results' / 'tables' / 'table2.csv'],
+        'uptodate': [True],  # Force re-download every time if equals to False
+        'clean': True,
         'verbosity': 0
     }
 
@@ -68,7 +78,19 @@ def task_transform_table1_to_latex():
     return {
         'actions': [(upload_table_download_latex, ['output/tables/table1.csv', 'table1'])],
         "file_dep": [BASE_DIR / 'output' / 'tables' / 'table1.csv'],
-        'targets': [BASE_DIR / 'results' / 'tables' / 'table1.tex'],
+        'targets': [BASE_DIR / 'output' / 'tables' / 'table1.tex'],
+        'uptodate': [False],  # Force to generate table1 every time
+        'clean': True,
+        'verbosity': 0
+    }
+
+
+def task_transform_table2_to_latex():
+    """Generate table1.csv from the downloaded data."""
+    return {
+        'actions': [(upload_table_download_latex, ['output/tables/table2.csv', 'table2'])],
+        "file_dep": [BASE_DIR / 'output' / 'tables' / 'table2.csv'],
+        'targets': [BASE_DIR / 'output' / 'tables' / 'table2.tex'],
         'uptodate': [False],  # Force to generate table1 every time
         'clean': True,
         'verbosity': 0
