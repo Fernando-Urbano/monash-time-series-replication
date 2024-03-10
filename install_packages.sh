f4ad7468ab839360884031a9084ff6ec5d20e65b
Fernando Rocha Urbano <fernando.rocha.urbano@gmail.com>
2024-03-10 10:28:10 -0400

    changed installation procedure to make sure it accounts for os

--- install_packages.sh
+++ install_packages.sh
@@ -2,10 +2,19 @@ if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
 
-# Install R packages from requirements_r.txt
-while IFS= read -r line; do
-    conda install -c conda-forge $line -y
-done < requirements_r.txt
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    while IFS= read -r line; do
+        conda install -c conda-forge "$line" -y
+    done < requirements_r_mac.txt
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    while IFS= read -r line; do
+        conda install -c conda-forge "$line" -y
+    done < requirements_r_linux.txt
+elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* || "$OSTYPE" == "win32"* ]]; then
+    while IFS= read -r line; do
+        conda install -c conda-forge "$line" -y
+    done < requirements_r_windows.txt
+fi
 
 # Install Python packages from requirements_py.txt
 pip install -r requirements_py.txt
