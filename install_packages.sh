9dd026bbf39c83173846af470d34105b0ae9593a
Fernando Rocha Urbano <fernando.rocha.urbano@gmail.com>
2024-03-09 20:28:45 -0500

    test

--- install_packages.sh
+++ install_packages.sh
@@ -2,10 +2,10 @@ if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
 
-# Install Python packages from requirements_py.txt
-pip install -r requirements_py.txt
-
 # Install R packages from requirements_r.txt
 while IFS= read -r line; do
     conda install -c conda-forge $line -y
 done < requirements_r.txt
+
+# Install Python packages from requirements_py.txt
+pip install -r requirements_py.txt
